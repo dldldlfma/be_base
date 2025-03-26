30668f50b793daa37774cd1d5c06ac9841b9ae7d
Sanggeun Kim <sgkim@superb-ai.com>
2025-03-26 17:14:30 +0900

    add db_session code

---
+++ app/db/session.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
+
+# Create DB Connect Engine
+engine = create_async_engine(settings.database_url,echo=True,future=True)
+
+# Create Session Factory
+async_session = sessionmaker(
+    bine=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+async def get_db():
+    async with async_session() as session:
+        yield session
+        
